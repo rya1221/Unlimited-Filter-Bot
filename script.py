e8cc37f8fbf178a72f9dbf6a80cbc8a33c9cabb6
rya1221 <91537646+rya1221@users.noreply.github.com>
2023-05-30 12:28:16 +0530

    Update script.py

--- script.py
+++ script.py
@@ -48,13 +48,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @GreyMatter_Bots</b>
+<b>© @friends_forever_chatting_world</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : ARYA Filter Bot</b>
 
-⭕️<b>Creater :</b> @GreyMatter_Bots    
+⭕️<b>Creater :</b>    @friends_forever_chatting_world
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
